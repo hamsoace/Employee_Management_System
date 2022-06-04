dcf3b2d73a707cb01844fd4af78f6fa6f0b601ef
hamsoace <hamsoace@gmail.com>
2022-06-04 11:52:10 +0300

    Pagination

--- src/main/java/com/hamso/ems/service/EmployeeService.java
+++ src/main/java/com/hamso/ems/service/EmployeeService.java
@@ -2,10 +2,14 @@ package com.hamso.ems.service;
 
 
 import com.hamso.ems.model.Employee;
+import org.springframework.data.domain.Page;
 
 import java.util.List;
 
 public interface EmployeeService {
     List<Employee> getAllEmployees();
     void saveEmployee(Employee employee);
+    Employee getEmployeeById(long id);
+    void deleteEmployeeById(long id);
+    Page<Employee> findPaginated(int pageNo, int pageSize);
 }
--- src/main/java/com/hamso/ems/service/EmployeeServiceImpl.java
+++ src/main/java/com/hamso/ems/service/EmployeeServiceImpl.java
@@ -3,9 +3,13 @@ package com.hamso.ems.service;
 import com.hamso.ems.model.Employee;
 import com.hamso.ems.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService{
@@ -22,4 +26,29 @@ public class EmployeeServiceImpl implements EmployeeService{
     public void saveEmployee(Employee employee) {
         this.employeeRepository.save(employee);
     }
+
+    @Override
+    public Employee getEmployeeById(long id) {
+        Optional<Employee>optional=employeeRepository.findById(id);
+        Employee employee = null;
+        if (optional.isPresent()){
+            employee= optional.get();
+        }else {
+            throw new RuntimeException("Employee not found for id :: " + id);
+        }
+        return employee;
+    }
+
+    @Override
+    public void deleteEmployeeById(long id) {
+        this.employeeRepository.deleteById(id);
+    }
+
+    @Override
+    public Page<Employee> findPaginated(int pageNo, int pageSize) {
+        Pageable pageable = PageRequest.of(pageNo - 1, pageSize);
+        return this.employeeRepository.findAll(pageable);
+    }
+
+
 }
